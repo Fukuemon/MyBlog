f50711cb127eb1eb1c010b2ff165d0516e2df420
Fukuemon <Fukuemony029@gmail.com>
2023-10-13 15:32:33 +0900

    🎨 Linterによるファイルの整形 #1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,31 @@
-import type { Config } from "tailwindcss";
-const colors = require("tailwindcss/colors");
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       fontFamily: {
         // font-**で呼び出すclass名 : フォント指定の配列
-        "source-code-pro": ["Source Code Pro", "monospace"],
-        burtons: ["Burtons", "cursive"],
+        'source-code-pro': ['Source Code Pro', 'monospace'],
+        burtons: ['Burtons', 'cursive']
       },
       colors: {
-        intro: "#fffdf8",
-        back: "#fdfbef",
-        main: "#d8bd97",
-        text: "#493c33",
+        intro: '#fffdf8',
+        back: '#fdfbef',
+        main: '#d8bd97',
+        text: '#493c33'
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
